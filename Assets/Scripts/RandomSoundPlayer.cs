cf6e11ebc0d531199db977b901530ab49f9563db
froostel <marius.a.irimia@gmail.com>
2025-03-05 14:33:51 +0100

    SFX Organization and base script created
    moved the SFX out of the music folder 💀

---
+++ Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    [Range(0f, 1f)]
+    public float clipVolume = 1.0f;
+
+
+
+    //On _ Trigger:
+        // //choose random clip
+        // AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
+
+        // //play audio clip
+        // audioSource.PlayOneShot(randomClip, clipVolume);
+}
