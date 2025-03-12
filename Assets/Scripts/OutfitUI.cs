b0b63fbfd206f70d0d6ba0bd0fdf6d8787adc48f
Julius Struijk <julius.struijk@gmail.com>
2025-03-12 12:04:13 +0100

    Trying to fix buttons

--- Assets/Scripts/OutfitUI.cs
+++ Assets/Scripts/OutfitUI.cs
@@ -6,16 +6,22 @@ public class UIOutfit : MonoBehaviour
 {
     GameObject pet;
     GameObject currentOutfit;
+    [SerializeField] GameObject customizationUI;
 
     // Start is called before the first frame update
     void Start()
     {
         pet = GameObject.FindGameObjectWithTag("Pet");
+        customizationUI.SetActive(false);
     }
 
     public void ChangeOutfit(GameObject newOutfit)
     {
+        Debug.Log("Changing outfit.");
         if(currentOutfit != null) { Destroy(currentOutfit); }
         if(newOutfit != null) { currentOutfit = Instantiate(newOutfit, pet.transform.position, newOutfit.transform.rotation); }
+        customizationUI.SetActive(false);
     }
+
+    public void EnableUI() { customizationUI.SetActive(true); }
 }
